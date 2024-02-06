a6b47324058cc34ea037f2d498d12ecd2a961fb0
Marius Meier <marius.meier24@gmail.com>
2024-02-06 15:45:46 +0200

    update

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: mmeier <mmeier@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 12:39:08 by mmeier            #+#    #+#             */
-/*   Updated: 2024/02/01 11:07:15 by mmeier           ###   ########.fr       */
+/*   Updated: 2024/02/06 15:45:25 by mmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,14 @@
 # include <unistd.h>
 
 int		ft_atoi(const char *str);
-//int		ft_isdigit(int n);
 char	*ft_strdup(const char *s1);
 size_t	ft_strlen(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	**ft_split(char const *s, char c);
+int     ft_check_input(char **av, int i);
+int	    input_is_nbr(char *av);
+int     is_sign(char c);
+int	    ft_isdigit_ps(char c);
 
 typedef struct s_list
 {
